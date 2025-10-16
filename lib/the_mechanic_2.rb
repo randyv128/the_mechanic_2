c5f22f5d151e57547ba48521bfa1abeba93b5c85
mrpandapants <villanuevarandy237@gmail.com>
2025-10-16 13:00:55 -0700

    Restructure to use Ruby autoload conventions
    - Moved all classes from app/ to lib/the_mechanic_2/
    - Added explicit autoload declarations in main module
    - Follows proper Ruby naming conventions for autoloading
    - Works with both eager_load and autoload modes
    - Removed dependency on Zeitwerk conventions

--- lib/the_mechanic_2.rb
+++ lib/the_mechanic_2.rb
@@ -3,5 +3,25 @@ require "the_mechanic_2/configuration"
 require "the_mechanic_2/engine"
 
 module TheMechanic2
-  # Your code goes here...
+  # Explicitly autoload all classes to work with both eager_load and autoload
+  autoload :ApplicationController, 'the_mechanic_2/application_controller'
+  autoload :BenchmarksController, 'the_mechanic_2/benchmarks_controller'
+  
+  autoload :BenchmarkService, 'the_mechanic_2/benchmark_service'
+  autoload :RailsRunnerService, 'the_mechanic_2/rails_runner_service'
+  autoload :SecurityService, 'the_mechanic_2/security_service'
+  
+  autoload :BenchmarkRequest, 'the_mechanic_2/benchmark_request'
+  autoload :BenchmarkResult, 'the_mechanic_2/benchmark_result'
+  
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  self.configuration = Configuration.new
 end
--- lib/the_mechanic_2/engine.rb
+++ lib/the_mechanic_2/engine.rb
@@ -8,11 +8,5 @@ module TheMechanic2
       g.fixture_replacement :factory_bot
       g.factory_bot dir: 'spec/factories'
     end
-    
-    # Rails will automatically load from these paths
-    # Just ensure they're in the engine's load path
-    config.eager_load_paths << root.join('app', 'controllers')
-    config.eager_load_paths << root.join('app', 'services')
-    config.eager_load_paths << root.join('app', 'models')
   end
 end
